03b6d108977f064dca22f27389b938f04a7e421d
Alston Chan <alstonchan64@berkeley.edu>
2019-03-23 12:00:22 -0700

    input info and it will appear inside database

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Test Site</h1>
+<form action="insert.php" method="post">
+Name: <input type="text" name="fname" /><br><br>
+Phone Numer: <input type="int" name="fphonenumber" /><br><br>
+Location: <input type="text" name="flocation" /><br><br>
+Time: <input type="text" name="ftime" /><br><br>
+
+<input type="submit" />
+</form>
+</body>
+</html>
