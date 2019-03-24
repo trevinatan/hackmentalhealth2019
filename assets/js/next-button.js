31a82c4a3ec78948d379f965d9fe9967e635ee3b
Trevina Tan <trevinatan@berkeley.edu>
2019-03-24 08:14:29 -0700

    does not pop

--- assets/js/next-button.js
+++ assets/js/next-button.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 $("#next").click(function() {window.scrollTo(0, $("#main-info").offset().top);
 });
 
-$("#next-info").click(function() {alert("pop");
+$("#next-info").click(function() {return null;
 });
 
 // Not used
