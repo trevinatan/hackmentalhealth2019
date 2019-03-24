62d4a3f1ec2fb38da66e604e41530ff6acbc0b52
Trevina Tan <trevinatan@berkeley.edu>
2019-03-24 05:55:02 -0700

    Smooth scroll works

---
+++ assets/js/next-button.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  // alert("pop");
+
+  var info = document.getElementById("main-info");
+  var next = document.getElementById("next");
+
+$("#next").click(function() {window.scrollTo(0, $("#main-info").offset().top);
+});
+
+$("#next-info").click(function() {alert("pop");
+});
+
+// Not used
+function scrollFunc() {
+  info.scrollIntoView();
+}
+
+
+});
